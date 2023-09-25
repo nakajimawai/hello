b518166b33327c9501556e52fb75a010352c0aea
nakajima <yutonkjima1108@gmail.com>
2023-09-25 15:52:08 +0900

    2回目のコミット

--- hello/hello_node.py
+++ hello/hello_node.py
@@ -1,9 +1,27 @@
 import rclpy
+import socket
 from rclpy.node import Node
 
 def todo():
-    while True:
-        print("hello world")
+    host_ip = '192.168.1.102'
+    host_port = 12345
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind((host_ip, host_port))
+
+    server_socket.listen(1)
+
+    print("待機中")
+
+    client_socket, client_address = server_socket.accept()
+
+    data = client_socket.recv(1024)
+
+    print("受信メッセージ：", data.decode())
+
+    client_socket.close()
+    server_socket.close()
 
 def main():
     rclpy.init()
